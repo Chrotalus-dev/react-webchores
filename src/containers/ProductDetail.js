0890039fda604fa93c2aa86b22d3ecf870049ca4
Julio Moreno <julioc.morenob@gmail.com>
2021-08-26 19:48:02 -0400

    Last push

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -11,11 +11,24 @@ const ProductDetail =()=>{
     const dispatch = useDispatch();
     console.log(product);
     
-    const fetchProductDetail = async () =>{
-        const response = await axios.get("https://rawg.io/api/games/"+{productId}+"?key=41e6a8e65b064f30b3221cc9ee70f3f6").catch(err=>{
-            console.log("Err ",err);
-        });
-        dispatch(selectedProduct(response.data));
+    // const fetchProductDetail = async () =>{
+    //     const response = await axios.get(https://rawg.io/api/games/{productId}?key=41e6a8e65b064f30b3221cc9ee70f3f6).catch(err=>{
+    //         console.log("Err ",err);
+    //     });
+    const fetchProductDetail= async()=>{
+   
+        var  options = {
+            method: 'GET',
+            url: "https://rawg.io/api/games/"+{productId}+"?key=41e6a8e65b064f30b3221cc9ee70f3f6",
+               };
+               console.log(options.url);
+      await axios.request(options).then(function  (response) {
+           dispatch(selectedProduct(response.data));
+          }).catch(function (error) {
+              console.error(error);
+              console.log(productId);
+          });
+        
     };
     useEffect(()=>{
         if(productId && productId !=="") fetchProductDetail();
